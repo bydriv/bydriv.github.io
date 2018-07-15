9a218f2e76b29b67c177dc5f0c0e658edbcaec01
bydriv <bydriv@gmail.com>
2018-07-15 16:01:23 +0900

    tmp..

--- books/techbookfest0005.v
+++ books/techbookfest0005.v
@@ -1,4 +1,5 @@
 Require Import Coq.Arith.Peano_dec.
+Require Import Coq.Arith.Lt.
 Require Import Coq.Lists.ListSet.
 
 Definition var : Type := nat.
@@ -14,6 +15,20 @@ Fixpoint length (e1 : term var) : nat :=
     | term_app e2 e3 => length e2 + length e3
     | term_abs f => S (length (f 0))
   end.
+
+Lemma term_len : forall e1, 0 < length e1.
+Proof.
+  intro e1.
+  induction e1.
+  (* e1 = term_var var v *)
+  auto.
+  (* e1 = term_app var e1_1 e1_2 *)
+  simpl. rewrite (S_pred (length e1_1) 0 IHe1_1). rewrite (S_pred (length e1_2) 0 IHe1_2).
+  simpl. apply neq_0_lt. auto.
+  (* e1 = term_abs var t *)
+  (* t : var -> term var *)
+  simpl. apply neq_0_lt. auto.
+Qed.
 
 Fixpoint FV' (xn : var) (e1 : term var) : set var :=
   match e1 with
@@ -64,3 +79,40 @@ Proof.
   (* t : var -> term var *)
   simpl. apply f_equal. apply (H 0).
 Qed.
+
+Fixpoint alpha_congruence' (xn : var) (e1 : term var) (e2 : term var) : Prop :=
+  match (e1, e2) with
+    | (term_var x1, term_var x2) =>
+      x1 = x2
+    | (term_app e3 e4, term_app e5 e6) =>
+      alpha_congruence' xn e3 e5 /\ alpha_congruence' xn e4 e6
+    | (term_abs f, term_abs g) =>
+      alpha_congruence' (S xn) (f xn) (g xn)
+    | _ =>
+      False
+  end.
+
+Definition alpha_congruence : term var -> term var -> Prop := alpha_congruence' 0.
+
+Lemma alpha_refl : forall e1, alpha_congruence e1 e1.
+Proof.
+  intro e1.
+  induction e1.
+  (* e1 = term_var var v *)
+  unfold alpha_congruence. simpl. reflexivity.
+  (* e1 = term_app var e1_1 e1_2 *)
+  unfold alpha_congruence. simpl. split. fold alpha_congruence. apply IHe1_1. fold alpha_congruence. apply IHe1_2.
+  (* e1 = term_abs var t *)
+  (* t : var -> term var *)
+  unfold alpha_congruence. simpl. Check (H 0).
+  (*remember (length e1) as n.
+  generalize dependent e1.
+  induction n.
+  (* length e1 = 0 *)
+  intros e1 Heqn. contradiction (lt_0_neq (length e1) (term_len e1)).
+  (* length e1 = S n *)
+  intros e1 Heqn. destruct e1.
+  (* e1 = term_var var v *)
+  unfold alpha_congruence. simpl. reflexivity.
+  (* e1 = term_app var e1_1 e1_2 *)
+  unfold alpha_congruence. simpl.*)
