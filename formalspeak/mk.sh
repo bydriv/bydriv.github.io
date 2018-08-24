376ce9b8ad64e2859b846550233d7fcc8844943d
bydriv <bydriv@gmail.com>
2018-08-24 20:27:08 +0900

    nouns

--- formalspeak/mk.sh
+++ formalspeak/mk.sh
@@ -9,5 +9,5 @@ echo '%%' >> common.l
 echo > formalspeak.y
 awk -F'\t' '{print $1}' common.tsv | sort | uniq | perl -pe 's/,/%token COMMA/;s/\./%token PERIOD/;s/:/%token COLON/;s/PRP\$/%token PRPD/;s/WP\$/%token WPD/;s/^([^%].*)$/%token $1/' >> formalspeak.y
 echo '%%' >> formalspeak.y
-perl -pe 's/=/:/' formalspeak.ebnf >> formalspeak.y
+perl -pe 's/=/:/;s/\(\*/\/*/;s/\*\)/*\//' formalspeak.ebnf >> formalspeak.y
 echo '%%' >> formalspeak.y
