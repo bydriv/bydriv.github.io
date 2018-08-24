46200bb6d3d2d005ff3ffe24b60951a5f31a3764
bydriv <bydriv@gmail.com>
2018-08-24 15:30:48 +0900

    formalspeak/formalspeak.bnf

---
+++ formalspeak/mk.sh
@@ -0,0 +1,13 @@
+echo > common.l
+echo '%{' >> common.l
+echo '#include "y.tab.h"' >> common.l
+echo '%}' >> common.l
+echo '%%' >> common.l
+awk -F, '{print "\"" $2 "\" {return " ($1 == ":" ? "COLON" : $1) ";}"}' common.csv >> common.l
+echo '%%' >> common.l
+
+echo > formalspeak.y
+awk -F, '{print $1}' common.csv | sort | uniq | perl -pe 's/:/%token COLON/;s/PRP\$/%token PRPD/;s/WP\$/%token WPD/;s/^([^%].*)$/%token $1/' >> formalspeak.y
+echo '%%' >> formalspeak.y
+perl -pe 's/=/:/' formalspeak.bnf >> formalspeak.y
+echo '%%' >> formalspeak.y
