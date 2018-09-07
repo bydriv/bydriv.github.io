6290ce78c9c33d13f62edca1de4c1e3554ea3cc9
bydriv <bydriv@gmail.com>
2018-09-07 23:00:46 +0900

    diesel hello

---
+++ rust-hello/migrations/2018-09-07-131713_create_test_table/up.sql
@@ -0,0 +1,5 @@
+-- Your SQL goes here
+CREATE TABLE test_table (
+  id SERIAL PRIMARY KEY,
+  text VARCHAR NOT NULL
+)
--- rust-hello/src/main.rs
+++ rust-hello/src/main.rs
@@ -1,8 +1,45 @@
 extern crate actix_web;
 use actix_web::{server, App, HttpRequest};
 
-fn index(_req: &HttpRequest) -> &'static str {
-  "Hello world!"
+#[macro_use]
+extern crate diesel;
+extern crate dotenv;
+
+use diesel::prelude::*;
+use diesel::pg::PgConnection;
+use dotenv::dotenv;
+use std::env;
+
+#[derive(Queryable)]
+pub struct TestTable {
+  pub id: i32,
+  pub text: String,
+}
+
+table! {
+  test_table (id) {
+    id -> Integer,
+    text -> Text,
+  }
+}
+
+pub fn establish_connection() -> PgConnection {
+  dotenv().ok();
+
+  let database_url = env::var("DATABASE_URL")
+      .expect("DATABASE_URL must be set");
+  PgConnection::establish(&database_url)
+  .expect(&format!("Error connecting to {}", database_url))
+}
+
+//fn index(_req: &HttpRequest) -> &'static str {
+fn index(_req: &HttpRequest) -> String {
+  let connection = establish_connection();
+  let entry = test_table::table.find(1).get_result::<TestTable>(&connection);
+  match entry {
+    Ok(e) => e.text,
+    Err(_) => "error".to_owned(),
+  }
 }
 
 fn main() {
