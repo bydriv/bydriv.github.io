bb1bf32c4b078bc06dd10c887e67af8c306ace95
bydriv <bydriv@gmail.com>
2018-09-09 02:00:37 +0900

    create followings

--- rustter/src/models.rs
+++ rustter/src/models.rs
@@ -3,3 +3,9 @@ pub struct User
 { pub id : i64
 , pub screen_name : String
 , pub hash : i64 }
+
+#[derive(Queryable)]
+pub struct Following
+{ pub id : i64
+, pub user_id : i64
+, pub following_user_id : i64 }
--- rustter/src/schema.rs
+++ rustter/src/schema.rs
@@ -1,7 +1,20 @@
 table! {
+    followings (id) {
+        id -> Int8,
+        user_id -> Int8,
+        following_user_id -> Int8,
+    }
+}
+
+table! {
     users (id) {
         id -> Int8,
         screen_name -> Text,
         hash -> Int8,
     }
 }
+
+allow_tables_to_appear_in_same_query!(
+    followings,
+    users,
+);
