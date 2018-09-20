99a72b34abaa25b1b4c7b8be2f7afa5178eba42c
bydriv <bydriv@gmail.com>
2018-09-21 04:11:17 +0900

    game/hijack/pixelart/teiri/walk/back

--- game/hijack.js
+++ game/hijack.js
@@ -13,7 +13,11 @@ const ASSETS = [
     "hijack/pixelart/teiri/walk/front/0.png",
     "hijack/pixelart/teiri/walk/front/1.png",
     "hijack/pixelart/teiri/walk/front/2.png",
-    "hijack/pixelart/teiri/walk/front/3.png"
+    "hijack/pixelart/teiri/walk/front/3.png",
+    "hijack/pixelart/teiri/walk/back/0.png",
+    "hijack/pixelart/teiri/walk/back/1.png",
+    "hijack/pixelart/teiri/walk/back/2.png",
+    "hijack/pixelart/teiri/walk/back/3.png"
 ];
 
 function game_new(k) {
@@ -30,15 +34,26 @@ function game_new(k) {
                 app.stage.scale.set(8, 8);
                 app.renderer.backgroundColor = 0xC0C0C0;
 
-                const sprite = new PIXI.extras.AnimatedSprite([
+                const sprite1 = new PIXI.extras.AnimatedSprite([
                     PIXI.loader.resources["hijack/pixelart/teiri/walk/front/0.png"].texture,
                     PIXI.loader.resources["hijack/pixelart/teiri/walk/front/1.png"].texture,
                     PIXI.loader.resources["hijack/pixelart/teiri/walk/front/2.png"].texture,
                     PIXI.loader.resources["hijack/pixelart/teiri/walk/front/3.png"].texture
                 ]);
-                sprite.animationSpeed = 1/10;
-                sprite.play();
-                app.stage.addChild(sprite);
+                sprite1.animationSpeed = 1/10;
+                sprite1.play();
+                app.stage.addChild(sprite1);
+
+                const sprite2 = new PIXI.extras.AnimatedSprite([
+                    PIXI.loader.resources["hijack/pixelart/teiri/walk/back/0.png"].texture,
+                    PIXI.loader.resources["hijack/pixelart/teiri/walk/back/1.png"].texture,
+                    PIXI.loader.resources["hijack/pixelart/teiri/walk/back/2.png"].texture,
+                    PIXI.loader.resources["hijack/pixelart/teiri/walk/back/3.png"].texture
+                ]);
+                sprite2.x = 16;
+                sprite2.animationSpeed = 1/10;
+                sprite2.play();
+                app.stage.addChild(sprite2);
 
                 document.getElementById("game").appendChild(app.view);
 
