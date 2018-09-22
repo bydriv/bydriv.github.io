061f3c91637600bc4f7d748fcf09faa6bc4b27e3
bydriv <bydriv@gmail.com>
2018-09-22 09:24:12 +0900

    namespace

--- game/hijack.js
+++ game/hijack.js
@@ -1,7 +1,7 @@
 window.addEventListener("load", () => {
-    game_new().then(game => {
+    Game.create().then(game => {
         requestAnimationFrame(function step () {
-            game_step(game).then(next_game => {
+            Game.step(game).then(next_game => {
                 game = next_game;
                 requestAnimationFrame(step);
             });
@@ -10,7 +10,7 @@ window.addEventListener("load", () => {
 });
 
 window.addEventListener("resize", () => {
-    game_resize();
+    Game.resize();
 });
 
 window.addEventListener("keydown", (e) => {
@@ -111,159 +111,160 @@ const ASSETS = [
 
 const TEXTURES = {};
 
-async function game_new() {
-    PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
+const Game = {
+    create: async () => {
+        PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
 
-    await Promise.all([font_load(), assets_load()]);
+        await Promise.all([font_load(), assets_load()]);
 
-    game_resize();
+        Game.resize();
 
-    const app = new PIXI.Application({autoStart: false, width: 1920, height: 1280});
-    app.stage.scale.set(8, 8);
-    app.renderer.backgroundColor = 0xC0C0C0;
+        const app = new PIXI.Application({autoStart: false, width: 1920, height: 1280});
+        app.stage.scale.set(8, 8);
+        app.renderer.backgroundColor = 0xC0C0C0;
 
-    document.getElementById("game").appendChild(app.view);
+        document.getElementById("game").appendChild(app.view);
 
-    const teiri = await teiri_new(app);
+        const teiri = await Teiri.create(app);
 
-    return {
-        app: app,
-        objects: [teiri]
-    };
-}
+        return {
+            app: app,
+            objects: [teiri]
+        };
+    },
+    step: async (game) => {
+        const gamepad = navigator.getGamepads()[0];
 
-async function teiri_new(app) {
-    const sprite = new PIXI.extras.AnimatedSprite([
-        TEXTURES["hijack/pixelart/teiri/walk/front/0.png"],
-        TEXTURES["hijack/pixelart/teiri/walk/front/1.png"],
-        TEXTURES["hijack/pixelart/teiri/walk/front/2.png"],
-        TEXTURES["hijack/pixelart/teiri/walk/front/3.png"]
-    ]);
-    sprite.animationSpeed = 1/8;
-    sprite.play();
-    app.stage.addChild(sprite);
+        if (gamepad) {
+            INPUT.x = gamepad.axes[0];
+            INPUT.y = gamepad.axes[1];
+        }
 
-    return {
-        type: "teiri",
-        control: {
-            type: "playable",
-            state: null,
-            input: INPUT_EMPTY
+        for (var i = 0; i < game.objects.length; ++i)
+            await Game.Object.step(game, game.objects[i]);
+
+        game.app.renderer.render(game.app.stage);
+
+        return game;
+    },
+    resize: () => {
+        if (document.body.clientWidth < 480)
+            document.getElementById("game").setAttribute("class", "single");
+        else if (document.body.clientWidth < 960)
+            document.getElementById("game").setAttribute("class", "double");
+        else
+            document.getElementById("game").setAttribute("class", "quadruple");
+    },
+    Object: {
+        control: async (game, control) => {
+            switch (control.type) {
+            case "playable":
+                return {
+                    type: control.type,
+                    state: control.state,
+                    input: INPUT
+                };
+            default:
+                return {
+                    type: control.type,
+                    state: control.state,
+                    input: INPUT_EMPTY
+                };
+            }
         },
-        sprite: sprite,
-        direction: "front"
-    };
-}
+        step: async (game, object) => {
+            object.control = await Game.Object.control(game, object.control);
 
-async function teiri_step(game, object) {
-    const input = object.control.input;
-
-    if (input.y < -0.25) {
-        if (object.direction !== "back") {
-            object.direction = "back";
-            object.sprite.textures = [
-                TEXTURES["hijack/pixelart/teiri/walk/back/0.png"],
-                TEXTURES["hijack/pixelart/teiri/walk/back/1.png"],
-                TEXTURES["hijack/pixelart/teiri/walk/back/2.png"],
-                TEXTURES["hijack/pixelart/teiri/walk/back/3.png"]
-            ];
-            object.sprite.play();
-        }
-    } else if (input.y > 0.25) {
-        if (object.direction !== "front") {
-            object.direction = "front";
-            object.sprite.textures = [
-                TEXTURES["hijack/pixelart/teiri/walk/front/0.png"],
-                TEXTURES["hijack/pixelart/teiri/walk/front/1.png"],
-                TEXTURES["hijack/pixelart/teiri/walk/front/2.png"],
-                TEXTURES["hijack/pixelart/teiri/walk/front/3.png"]
-            ];
-            object.sprite.play();
-        }
-    } else if (input.x < -0.25) {
-        if (object.direction !== "left") {
-            object.direction = "left";
-            object.sprite.textures = [
-                TEXTURES["hijack/pixelart/teiri/walk/left/0.png"],
-                TEXTURES["hijack/pixelart/teiri/walk/left/1.png"],
-                TEXTURES["hijack/pixelart/teiri/walk/left/2.png"],
-                TEXTURES["hijack/pixelart/teiri/walk/left/3.png"]
-            ];
-            object.sprite.play();
-        }
-    } else if (input.x > 0.25) {
-        if (object.direction !== "right") {
-            object.direction = "right";
-            object.sprite.textures = [
-                TEXTURES["hijack/pixelart/teiri/walk/right/0.png"],
-                TEXTURES["hijack/pixelart/teiri/walk/right/1.png"],
-                TEXTURES["hijack/pixelart/teiri/walk/right/2.png"],
-                TEXTURES["hijack/pixelart/teiri/walk/right/3.png"]
-            ];
-            object.sprite.play();
+            switch (object.type) {
+            case "teiri":
+                await Teiri.step(game, object);
+                break;
+            }
         }
     }
+};
 
-    if (input.x < -0.25)
-        object.sprite.x -= 1;
-    if (input.x > 0.25)
-        object.sprite.x += 1;
-    if (input.y < -0.25)
-        object.sprite.y -= 1;
-    if (input.y > 0.25)
-        object.sprite.y += 1;
-}
+const Teiri = {
+    create: async (app) => {
+        const sprite = new PIXI.extras.AnimatedSprite([
+            TEXTURES["hijack/pixelart/teiri/walk/front/0.png"],
+            TEXTURES["hijack/pixelart/teiri/walk/front/1.png"],
+            TEXTURES["hijack/pixelart/teiri/walk/front/2.png"],
+            TEXTURES["hijack/pixelart/teiri/walk/front/3.png"]
+        ]);
+        sprite.animationSpeed = 1/8;
+        sprite.play();
+        app.stage.addChild(sprite);
 
-async function object_control(game, control) {
-    switch (control.type) {
-    case "playable":
         return {
-            type: control.type,
-            state: control.state,
-            input: INPUT
+            type: "teiri",
+            control: {
+                type: "playable",
+                state: null,
+                input: INPUT_EMPTY
+            },
+            sprite: sprite,
+            direction: "front"
         };
-    default:
-        return {
-            type: control.type,
-            state: control.state,
-            input: INPUT_EMPTY
-        };
+    },
+    step: async (game, object) => {
+        const input = object.control.input;
+
+        if (input.y < -0.25) {
+            if (object.direction !== "back") {
+                object.direction = "back";
+                object.sprite.textures = [
+                    TEXTURES["hijack/pixelart/teiri/walk/back/0.png"],
+                    TEXTURES["hijack/pixelart/teiri/walk/back/1.png"],
+                    TEXTURES["hijack/pixelart/teiri/walk/back/2.png"],
+                    TEXTURES["hijack/pixelart/teiri/walk/back/3.png"]
+                ];
+                object.sprite.play();
+            }
+        } else if (input.y > 0.25) {
+            if (object.direction !== "front") {
+                object.direction = "front";
+                object.sprite.textures = [
+                    TEXTURES["hijack/pixelart/teiri/walk/front/0.png"],
+                    TEXTURES["hijack/pixelart/teiri/walk/front/1.png"],
+                    TEXTURES["hijack/pixelart/teiri/walk/front/2.png"],
+                    TEXTURES["hijack/pixelart/teiri/walk/front/3.png"]
+                ];
+                object.sprite.play();
+            }
+        } else if (input.x < -0.25) {
+            if (object.direction !== "left") {
+                object.direction = "left";
+                object.sprite.textures = [
+                    TEXTURES["hijack/pixelart/teiri/walk/left/0.png"],
+                    TEXTURES["hijack/pixelart/teiri/walk/left/1.png"],
+                    TEXTURES["hijack/pixelart/teiri/walk/left/2.png"],
+                    TEXTURES["hijack/pixelart/teiri/walk/left/3.png"]
+                ];
+                object.sprite.play();
+            }
+        } else if (input.x > 0.25) {
+            if (object.direction !== "right") {
+                object.direction = "right";
+                object.sprite.textures = [
+                    TEXTURES["hijack/pixelart/teiri/walk/right/0.png"],
+                    TEXTURES["hijack/pixelart/teiri/walk/right/1.png"],
+                    TEXTURES["hijack/pixelart/teiri/walk/right/2.png"],
+                    TEXTURES["hijack/pixelart/teiri/walk/right/3.png"]
+                ];
+                object.sprite.play();
+            }
+        }
+
+        if (input.x < -0.25)
+            object.sprite.x -= 1;
+        if (input.x > 0.25)
+            object.sprite.x += 1;
+        if (input.y < -0.25)
+            object.sprite.y -= 1;
+        if (input.y > 0.25)
+            object.sprite.y += 1;
     }
-}
-
-async function object_step(game, object) {
-    object.control = await object_control(game, object.control);
-
-    switch (object.type) {
-    case "teiri":
-        await teiri_step(game, object);
-        break;
-    }
-}
-
-async function game_step(game) {
-    const gamepad = navigator.getGamepads()[0];
-
-    if (gamepad) {
-        INPUT.x = gamepad.axes[0];
-        INPUT.y = gamepad.axes[1];
-    }
-
-    for (var i = 0; i < game.objects.length; ++i)
-        await object_step(game, game.objects[i]);
-
-    game.app.renderer.render(game.app.stage);
-
-    return game;
-}
-
-function game_resize() {
-    if (document.body.clientWidth < 480)
-        document.getElementById("game").setAttribute("class", "single");
-    else if (document.body.clientWidth < 960)
-        document.getElementById("game").setAttribute("class", "double");
-    else
-        document.getElementById("game").setAttribute("class", "quadruple");
 }
 
 function assets_load() {
