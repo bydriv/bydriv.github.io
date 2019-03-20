b4b1ecd1b8658c5b8b1b1feee343e889da46c4b9
bydriv <bydriv@gmail.com>
2019-03-21 00:00:10 +0900

    wip

--- liljax/src/Liljax.elm
+++ liljax/src/Liljax.elm
@@ -1,3 +1,18 @@
+import Browser
 import Html exposing (div, text)
 import Html.Attributes exposing (class)
-main = div [class "liljax"] [text "hello world"]
+
+type alias Model = ()
+type alias Msg = ()
+
+main : Program () Model Msg
+main = Browser.sandbox {init = init, update = update, view = view}
+
+init : Model
+init = ()
+
+update : Msg -> Model -> Model
+update () () = ()
+
+view : Model -> Html.Html Msg
+view () = div [class "liljax"] [text "hello world"]
