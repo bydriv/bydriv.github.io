ceaf0d18682f58c29b49eb1c352d792958036b42
bydriv <bydriv@gmail.com>
2019-04-15 14:29:25 +0900

    wip

--- shirley/html.rb
+++ shirley/html.rb
@@ -7,7 +7,6 @@ cfg = Shirley.config([
     ["paragraph", /\A#(\S+)\s*(.*)\z/m, "<\\1>\\2</\\1>"],
     ["paragraph", /\A(.*)\z/m, "<p>\\1</p>"],
     ["inline", /\A#(\S+)\s*(.*)\z/m, "<\\1>\\2</\\1>"],
-    ["atom", /\A\\#/m, "&#x23;"],
     ["atom", /&/m, "&amp;"],
     ["atom", /\\/m, "&bsol;"],
     ["atom", /</m, "&lt;"],
