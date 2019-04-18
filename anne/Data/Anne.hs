5cc4611a03e5532c2633fb4989db68b94623e1fb
bydriv <bydriv@gmail.com>
2019-04-19 05:16:07 +0900

    report

--- anne/Data/Anne.hs
+++ anne/Data/Anne.hs
@@ -1,6 +1,6 @@
 module Data.Anne(
   module Data.Anne.Defn,
-  SyntaxError,
+  SyntaxError(..),
   parse
 ) where
 
@@ -11,22 +11,42 @@ import           Data.Anne.Defn
 import qualified Data.Anne.Parsing      as Parsing
 import qualified Data.Anne.Lexing       as Lexing
 
-data SyntaxError = SyntaxError
+data SyntaxError =
+    NonterminatingRawText Pos
+  | UnrecognizedCharacter Pos Char
+  | UnexpectedEOF Pos
+  | UnexpectedToken Pos Parsing.Token
   deriving (Eq, Ord, Read, Show)
+
+posOf :: Parsing.Token -> Pos
+posOf (Parsing.LBRACKET (p, _)) = p
+posOf (Parsing.RBRACKET (p, _)) = p
+posOf (Parsing.BLANK (p, _)) = p
+posOf (Parsing.TEXT (p, _)) = p
+posOf (Parsing.RAW1 (p, _, _)) = p
+posOf (Parsing.RAWN (p, _, _)) = p
 
 parse :: String -> Either SyntaxError Anne
 parse s =
-  let ((tokens0, s'), _) = flip State.runState (0, 0, 0, "", []) $ Lexing.runLexing $ Lexing.lex Lexing.semanticActions s in
+  let ((tokens0, s'), (_, bufP, bufN, k, _)) = flip State.runState (0, 0, 0, "", []) $ Lexing.runLexing $ Lexing.lex Lexing.semanticActions s in
   let tokens = Maybe.catMaybes tokens0 in
 
-  case s' of
+  case k of
     (_ : _) ->
-      Left SyntaxError
-    [] ->
-      case Identity.runIdentity $ Parsing.parse Parsing.semanticActions tokens of
-        Left Nothing ->
-          Left SyntaxError
-        Left (Just _) ->
-          Left SyntaxError
-        Right (result, _) ->
-          Right result
+      Left (NonterminatingRawText (bufP, bufP + bufN))
+    "" ->
+      case s' of
+        (c : _) ->
+          let p = length s - length s' in
+          let q = p + 1 in
+            Left (UnrecognizedCharacter (p, q) c)
+        [] ->
+          case Identity.runIdentity $ Parsing.parse Parsing.semanticActions tokens of
+            Left Nothing ->
+              let p = length s in
+              let q = p in
+                Left (UnexpectedEOF (p, q))
+            Left (Just t) ->
+              Left (UnexpectedToken (posOf t) t)
+            Right (result, _) ->
+              Right result
--- anne/anne.hs
+++ anne/anne.hs
@@ -1,5 +1,7 @@
-import qualified Data.Anne  as Anne
-import qualified Data.List  as List
+import qualified Data.Anne   as Anne
+import qualified Data.List   as List
+import qualified System.Exit as Exit
+import qualified System.IO   as IO
 
 escape :: String -> String
 escape [] = []
@@ -41,6 +43,46 @@ atomToJSON (Anne.RawN _ k s) = ["{\"type\":\"rawn\",\"delimiter\":\"", escape k,
 listToJSON :: Anne.List -> [String]
 listToJSON (Anne.List _ ds) = concat [["{\"type\":\"list\",\"value\":"], dataToJSON ds, ["}"]]
 
+beginningOfLine :: Anne.Cursor -> String -> Int
+beginningOfLine p s =
+  let s1 = take p s in
+  let s2 = takeWhile (\c -> c /= '\n' && c /= '\r') (reverse s1) in
+    p - length s2
+
+endOfLine :: Anne.Cursor -> String -> Int
+endOfLine p s =
+  let s1 = drop p s in
+  let s2 = takeWhile (\c -> c /= '\n' && c /= '\r') s1 in
+  case drop (length s2) s1 of
+    '\r':'\n':_ -> p + length s2 + 2
+    '\n':_ -> p + length s2 + 1
+    '\r':_ -> p + length s2 + 1
+    _ -> p + length s2
+
+lineOf :: Anne.Cursor -> String -> Int
+lineOf p =
+  let f i s =
+        let n = endOfLine 0 s in
+          if i < p + n then
+              f (i + n) (drop n s) + 1
+          else
+            0
+  in f 0
+
+colOf :: Anne.Cursor -> String -> Int
+colOf p s =
+  let bol = beginningOfLine p s in
+    (p - bol) + 1
+
+substr :: Anne.Pos -> String -> String
+substr (p, q) = take (q - p) . drop p
+
+highlight :: Anne.Pos -> String -> String
+highlight (p, q) s =
+  let (s1, s') = splitAt p s in
+    let (s2, s3) = splitAt (q - p) s' in
+    s1 ++ "\027[4m" ++ s2 ++ "\027[0m" ++ s3
+
 main :: IO ()
 main = do
   s <- getContents
@@ -49,4 +91,32 @@ main = do
     Right a ->
       putStrLn (toJSON a)
     Left e ->
-      print e
+      let report pos' msg =
+              let bol = beginningOfLine (fst pos') s in
+              let eol = endOfLine (snd pos') s in
+              let s'' = substr (bol, eol) s in
+              let lineno1 = lineOf (fst pos') s in
+              let colno1 = colOf (fst pos') s in
+              let lineno2 = lineOf (snd pos') s in
+              let colno2 = colOf (snd pos') s in
+              let loc =
+                    if lineno1 == lineno2 then
+                      if colno1 == colno2 then
+                        show lineno1 ++ ":" ++ show colno1
+                      else
+                        show lineno1 ++ ":" ++ show colno1 ++ "-" ++ show colno2
+                    else
+                      show lineno1 ++ ":" ++ show colno1 ++ "-" ++ show lineno2 ++ ":" ++ show colno2 in do
+                IO.hPutStrLn IO.stderr $ "\027[1m" ++ loc ++ ":\027[0m \027[1;31merror:\027[0m \027[1m" ++ msg ++ "\027[0m"
+                IO.hPutStr IO.stderr $ highlight (fst pos' - bol, snd pos' - bol) s''
+                Exit.exitFailure
+      in do
+        case e of
+          Anne.NonterminatingRawText (p, q) ->
+            report (p, q) "nonterminating raw text"
+          Anne.UnrecognizedCharacter (p, q) c ->
+            report (p, q) ("unrecognized character: " ++ [c])
+          Anne.UnexpectedEOF (p, q) ->
+            report (p, q) "unexpected EOF"
+          Anne.UnexpectedToken (p, q) t ->
+            report (p, q) ("unexpected token: " ++ show t)
