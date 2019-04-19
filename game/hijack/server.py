a05af661da87f96e75fcad54d15a14d440ab7151
bydriv <bydriv@gmail.com>
2019-04-20 05:47:51 +0900

    rm

--- game/hijack/server.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import http.server
-import socketserver
-
-http.server.SimpleHTTPRequestHandler.extensions_map[".wasm"] = "application/wasm"
-
-socketserver.TCPServer(("", 8000), http.server.SimpleHTTPRequestHandler).serve_forever()
