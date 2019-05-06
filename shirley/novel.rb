52b3a80b5cd8b771e6bc09a78cb20a395eecfc3c
bydriv <bydriv@gmail.com>
2019-05-06 13:28:02 +0900

    novel/selective

--- shirley/novel.rb
+++ shirley/novel.rb
@@ -10,12 +10,12 @@ cfg = Shirley.config([
     ["first-paragraph", /\A\$(\d+)\z/m, proc {|m| $chap = m[1]; "<h2 class=\"chap\" id=\"#{m[1]}\"><a href=\"##$chap\">第#{m[1]}章</a></h2>" }],
     ["first-paragraph", /\A\+\z/m, "<div class=\"blank-line\"></div>"],
     ["first-paragraph", /\A\*\z/m, "<div class=\"blank-line\"></div>"],
-    ["first-paragraph", /\A(<.*?>.*?<\/.*?>)(「.*)\z/m, "<p class=\"noindent\">\\1\\2</p>"],
+    ["first-paragraph", /\A(<sup[^>]*?>.*?<\/sup><span [^>]*>)(「.*)\z/m, "<p class=\"noindent\">\\1\\2</p>"],
     ["first-paragraph", /\A(.*)\z/m, "<p class=\"indent\">\\1</p>"],
     ["first-list", /\A(\d+)\z/m, proc {|m| "<sup class=\"sect\" id=\"#$chap:#{m[1]}\"><a href=\"##$chap:#{m[1]}\">#{m[1]}</a></sup>"}],
     ["first-list", /\A#ruby:([^:]+):([^:]+)\z/m, "<ruby>\\1<rp>(</rp><rt>\\2</rt><rp>)</rp></ruby>"],
-    ["all-text", /\n/m, ""],
     ["all-text", /(！|？|！？)([^！？」])/m, "\\1<span class=\"space\"></span>\\2"],
+    ["all-text", /\n/m, ""],
     ["all-text", /(……)/m, "<span class=\"dots\">\\1</span>"],
     ["all-text", /(――)/m, "<span class=\"dashes\">\\1</span>"],
     ["first-text", /\A\$(\d+):(.*?)\z/m, "\\0"],
@@ -23,6 +23,7 @@ cfg = Shirley.config([
     ["first-text", /\A\+\z/m, "\\0"],
     ["first-text", /\A\*\z/m, "\\0"],
     ["first-text", /\A(\d+)\z/m, "\\0"],
+    ["first-text", /\A#ruby:([^:]+):([^:]+)\z/m, "\\0"],
     ["first-text", /\A(.+)\z/m, "<span class=\"text\">\\1</span>"]
 ])
 
